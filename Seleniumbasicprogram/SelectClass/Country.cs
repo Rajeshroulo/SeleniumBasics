56b59aa4dc323dbe6ba92203dfab4621660055b4
Rajeshroulo <rajraval017@gmail.com>
2020-08-06 15:40:04 +0530

    Added Script to print the elements and it's count in a dropdown using select class

---
+++ Seleniumbasicprogram/SelectClass/Country.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Seleniumbasicprogram.SelectClass
+{
+   public class Country
+    {
+       IWebDriver driver;
+
+        [Test]
+        public void MultiselectDropdown()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "http://uitestpractice.com/Students/Select";
+            driver.Manage().Window.Maximize();
+            IWebElement dropdown = driver.FindElement(By.Id("countriesMultiple"));
+            SelectElement element = new SelectElement(dropdown);
+
+              IList<IWebElement> elements = element.Options;
+              Console.WriteLine(elements.Count);
+              foreach(var item in elements)
+              {
+                  Console.WriteLine(item.Text);
+              }
+
+           
+
+            driver.Quit();
+        }
+
+
+    }
+}
---
+++ Seleniumbasicprogram/SelectClass/Facebook.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Seleniumbasicprogram.SelectClass
+{
+  public class FaceBook
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://facebook.com");
+            driver.Manage().Window.Maximize();
+
+        }
+
+       
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+
+        
+    }
+}
