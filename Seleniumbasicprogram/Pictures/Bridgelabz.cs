175d7ec9676e58af9949ab8c76bc03eb6a57f92e
Rajeshroulo <rajraval017@gmail.com>
2020-08-06 17:51:10 +0530

    Added script to take screen shot of the required page

---
+++ Seleniumbasicprogram/Pictures/Bridgelabz.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seleniumbasicprogram.Pictures
+{
+  public class Bridgelabz
+  {
+      [Test]
+      public void Screenshot()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.bridgelabz.com/";
+            ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile("Bridgelabz.png", ScreenshotImageFormat.Png);
+            driver.Quit();
+        }
+
+
+  }
+}
