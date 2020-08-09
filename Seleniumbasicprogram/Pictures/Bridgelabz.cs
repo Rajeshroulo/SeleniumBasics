ee75d1ea141cbc82aa919a2533fb390ee7fc00c4
Rajeshroulo <rajraval017@gmail.com>
2020-08-09 17:22:59 +0530

    Added script to take screen shot when the test fails

--- Seleniumbasicprogram/Pictures/Bridgelabz.cs
+++ Seleniumbasicprogram/Pictures/Bridgelabz.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,17 +12,52 @@ namespace Seleniumbasicprogram.Pictures
 {
   public class Bridgelabz
   {
-      [Test]
+
+        public IWebDriver driver;  
+      [Test,Author("Rajesh","rajraval017@gmail.com")]
+      [Description("Take screenshot")]
       public void Screenshot()
       {
-            IWebDriver driver = new ChromeDriver();
+            driver = new ChromeDriver();
             driver.Url = "https://www.bridgelabz.com/";
             driver.Manage().Window.Maximize();
-            ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile("Bridgelab.jpeg", ScreenshotImageFormat.Jpeg);
+            ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile(@"C:\Users\HP\source\repos\Seleniumbasicprogram\Seleniumbasicprogram\Pictures\\Bridgelab.jpeg", ScreenshotImageFormat.Jpeg);
             driver.Quit();
       }
 
-     
+      [Test, Author("Rajesh", "rajraval017@gmail.com")]
+      [TestCaseSource("Datalinks")]
+      public void SourceTest(String url)
+        {
+            try
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Url = url;
+                driver.FindElement(By.Id("email")).SendKeys("9736373828");
+                driver.Quit();
+            }
+                
+            catch(Exception e)
+            {
+                ITakesScreenshot sc = driver as ITakesScreenshot;
+                Screenshot st = sc.GetScreenshot();
+                st.SaveAsFile(@"C:\Users\HP\source\repos\Seleniumbasicprogram\Seleniumbasicprogram\Pictures\\fb.jpeg", ScreenshotImageFormat.Jpeg);
+                Console.WriteLine(e.StackTrace);
+                driver.Quit();
+                throw;
+            }
 
-  }
+        }
+
+        static IList Datalinks()
+        {
+            ArrayList list = new ArrayList();
+            list.Add("https://www.facebook.com");
+            return list;
+
+        } 
+
+
+    }
 }
