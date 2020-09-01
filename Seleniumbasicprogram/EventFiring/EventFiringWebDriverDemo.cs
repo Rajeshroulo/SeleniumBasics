1392d34edb08ad10b28bd2ff8b6545a2e68045a0
Rajeshroulo <rajraval017@gmail.com>
2020-09-01 10:16:01 +0530

    Added script for Event firing web driver class

---
+++ Seleniumbasicprogram/EventFiring/EventFiringWebDriverDemo.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Events;
+using System;
+
+namespace Seleniumbasicprogram.EventFiring
+{
+    public class EventFiringWebDriverDemo
+    {
+        public IWebDriver driver;
+
+        [Test]
+        public void EventFiringDemo()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "http://uitestpractice.com";
+            driver.Manage().Window.Maximize();
+            EventFiringWebDriver eventFiringWebDriver = new EventFiringWebDriver(driver);
+
+            //Click events
+            eventFiringWebDriver.ElementClicking += EventFiringWebDriver_ElementClicking;
+            eventFiringWebDriver.ElementClicked += EventFiringWebDriver_ElementClicked;
+            driver = eventFiringWebDriver;
+            driver.FindElement(By.LinkText("Form")).Click();
+
+            driver.Quit();
+        }
+
+        private void EventFiringWebDriver_ElementClicked(object sender, WebElementEventArgs e)
+        {
+            Console.WriteLine("Element Clicked");  
+        }
+
+        private void EventFiringWebDriver_ElementClicking(object sender, WebElementEventArgs e)
+        {
+            Console.WriteLine("Element clicking");
+        }
+    }
+}
