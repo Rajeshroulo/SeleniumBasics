feb04ee615f4d6164daf6d03f81b4b8f596af901
Rajeshroulo <rajraval017@gmail.com>
2020-09-01 11:47:50 +0530

    Added code for element value changing events in event firing web driver class

--- Seleniumbasicprogram/EventFiring/EventFiringWebDriverDemo.cs
+++ Seleniumbasicprogram/EventFiring/EventFiringWebDriverDemo.cs
@@ -21,10 +21,28 @@ namespace Seleniumbasicprogram.EventFiring
             //Click events
             eventFiringWebDriver.ElementClicking += EventFiringWebDriver_ElementClicking;
             eventFiringWebDriver.ElementClicked += EventFiringWebDriver_ElementClicked;
+            
+            //Element value changing events
+            eventFiringWebDriver.ElementValueChanging += EventFiringWebDriver_ElementValueChanging;
+            eventFiringWebDriver.ElementValueChanged += EventFiringWebDriver_ElementValueChanged;
+
             driver = eventFiringWebDriver;
             driver.FindElement(By.LinkText("Form")).Click();
+            driver.FindElement(By.Id("firstname")).SendKeys("Rajesh kumar");
+
+
 
             driver.Quit();
+        }
+
+        private void EventFiringWebDriver_ElementValueChanged(object sender, WebElementValueEventArgs e)
+        {
+            Console.WriteLine("Element value changed");
+        }
+
+        private void EventFiringWebDriver_ElementValueChanging(object sender, WebElementValueEventArgs e)
+        {
+            Console.WriteLine("Element value changing");
         }
 
         private void EventFiringWebDriver_ElementClicked(object sender, WebElementEventArgs e)
