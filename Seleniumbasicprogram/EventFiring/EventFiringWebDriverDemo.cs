3fb9698d543ab45a8e27e4d93e4eaf09b802075e
Rajeshroulo <rajraval017@gmail.com>
2020-09-01 12:12:06 +0530

    Added code for navigation events in event firing web driver

--- Seleniumbasicprogram/EventFiring/EventFiringWebDriverDemo.cs
+++ Seleniumbasicprogram/EventFiring/EventFiringWebDriverDemo.cs
@@ -26,13 +26,60 @@ namespace Seleniumbasicprogram.EventFiring
             eventFiringWebDriver.ElementValueChanging += EventFiringWebDriver_ElementValueChanging;
             eventFiringWebDriver.ElementValueChanged += EventFiringWebDriver_ElementValueChanged;
 
+            //Navigation events
+            eventFiringWebDriver.Navigating += EventFiringWebDriver_Navigating;
+            eventFiringWebDriver.Navigated += EventFiringWebDriver_Navigated;
+
+            eventFiringWebDriver.NavigatingForward += EventFiringWebDriver_NavigatingForward;
+            eventFiringWebDriver.NavigatedForward += EventFiringWebDriver_NavigatedForward;
+
+            eventFiringWebDriver.NavigatingBack += EventFiringWebDriver_NavigatingBack;
+            eventFiringWebDriver.NavigatedBack += EventFiringWebDriver_NavigatedBack;
+            
             driver = eventFiringWebDriver;
+
+            //Click events will trigger here
             driver.FindElement(By.LinkText("Form")).Click();
+
+            //Element value change event
             driver.FindElement(By.Id("firstname")).SendKeys("Rajesh kumar");
 
-
+            //Navigation events
+            driver.Navigate().GoToUrl("http://uitestpractice.com");
+            driver.Navigate().Back();
+            driver.Navigate().Forward();
 
             driver.Quit();
+        }
+
+        private void EventFiringWebDriver_NavigatedBack(object sender, WebDriverNavigationEventArgs e)
+        {
+            Console.WriteLine(" Navigated Back");
+        }
+
+        private void EventFiringWebDriver_NavigatingBack(object sender, WebDriverNavigationEventArgs e)
+        {
+            Console.WriteLine(" Navigating Back");
+        }
+
+        private void EventFiringWebDriver_NavigatedForward(object sender, WebDriverNavigationEventArgs e)
+        {
+            Console.WriteLine(" Navigated forward");
+        }
+
+        private void EventFiringWebDriver_NavigatingForward(object sender, WebDriverNavigationEventArgs e)
+        {
+            Console.WriteLine(" Navigating forward");
+        }
+
+        private void EventFiringWebDriver_Navigated(object sender, WebDriverNavigationEventArgs e)
+        {
+            Console.WriteLine("Element  Navigated");
+        }
+
+        private void EventFiringWebDriver_Navigating(object sender, WebDriverNavigationEventArgs e)
+        {
+            Console.WriteLine("Element  Navigating");
         }
 
         private void EventFiringWebDriver_ElementValueChanged(object sender, WebElementValueEventArgs e)
